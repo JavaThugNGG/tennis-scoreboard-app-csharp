42b3277ff5a5c41754e235038db3e655c1e4988b
JavaThugNGG <ildarjava@mail.ru>
2025-09-14 20:44:27 +0300

    feat(exception): add PlayerAlreadyExistsException

---
+++ TennisScoreboard/PlayerAlreadyExistsException.cs
@@ -0,0 +1,7 @@
+﻿namespace TennisScoreboard
+{
+    public class PlayerAlreadyExistsException : Exception
+    {
+        public PlayerAlreadyExistsException(string message) : base(message) { }
+    }
+}
