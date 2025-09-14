727be0cfa2bb3d04dcb0ae8e437dcc5b057d4cc7
JavaThugNGG <ildarjava@mail.ru>
2025-09-14 20:31:24 +0300

    feat(builder): add ErrorDtoBuilder

---
+++ TennisScoreboard/ErrorDtoBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace TennisScoreboard
+{
+    public class ErrorDtoBuilder
+    {
+        private readonly StatusCodeProcessor _statusCodeProcessor;
+
+        public ErrorDtoBuilder(StatusCodeProcessor statusCodeProcessor)
+        {
+            _statusCodeProcessor = statusCodeProcessor;
+        }
+
+        public ErrorDto Build(Exception exception)
+        {
+            int statusCode = _statusCodeProcessor.ResolveStatusCode(exception);
+            string message = "ошибка: " + exception.Message;
+            return new ErrorDto(statusCode, message);
+        }
+    }
+}
