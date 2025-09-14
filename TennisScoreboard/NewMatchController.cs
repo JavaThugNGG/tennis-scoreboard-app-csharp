e6ff77c6d0235b955c5e5891a6b2d6568ecc0a3c
JavaThugNGG <ildarjava@mail.ru>
2025-09-14 19:34:49 +0300

    feat: add controller and view for new match creation

---
+++ TennisScoreboard/NewMatchController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TennisScoreboard
+{
+    public class NewMatchController : Controller
+    {
+        [HttpGet("/new-match")]
+        public IActionResult ShowForm()
+        {
+            return View("NewMatch");
+        }
+    }
+}
