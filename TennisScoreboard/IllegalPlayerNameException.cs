9189aeba90e4bd1db6db7481745a115b2fe51855
JavaThugNGG <ildarjava@mail.ru>
2025-09-14 20:09:36 +0300

    feat(exception): add IllegalPlayerNameException

---
+++ TennisScoreboard/IllegalPlayerNameException.cs
@@ -0,0 +1,7 @@
+﻿namespace TennisScoreboard
+{
+    public class IllegalPlayerNameException : Exception
+    {
+        public IllegalPlayerNameException(string message) : base(message) { }
+    }
+}
