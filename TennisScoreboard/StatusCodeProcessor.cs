31808995eee8abd19ecbeaa91d8e1136f0b6fcfb
JavaThugNGG <ildarjava@mail.ru>
2025-09-14 20:31:51 +0300

    feat(processor): add StatusCodeProcessor

---
+++ TennisScoreboard/StatusCodeProcessor.cs
@@ -0,0 +1,14 @@
+﻿namespace TennisScoreboard
+{
+    public class StatusCodeProcessor
+    {
+        public int ResolveStatusCode(Exception exception)
+        {
+            return exception switch
+            {
+                ArgumentException => StatusCodes.Status400BadRequest,
+                _ => StatusCodes.Status500InternalServerError
+            };
+        }
+    }
+}
