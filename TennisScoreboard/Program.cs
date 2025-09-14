958247debc39d6e187a9227ed7a3182d8ed52ecd
JavaThugNGG <ildarjava@mail.ru>
2025-09-14 16:06:30 +0300

    chore(infrastructure): add index.html as default page

--- TennisScoreboard/Program.cs
+++ TennisScoreboard/Program.cs
@@ -1,23 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace TennisScoreboard
 {
-    app.UseExceptionHandler("/Home/Error");
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            /*
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }*/
+
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();//сопоставляет url с маршрутами(готовься к маршрутизации)
+
+            app.MapControllerRoute(//определение шаблона маршрутов(вместе это все позволяет правильно направлять запросы на нужные ендпоинты)
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-app.UseStaticFiles();
 
-app.UseRouting();
 
-app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+
+
