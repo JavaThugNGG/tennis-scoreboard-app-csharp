086acf4f3179ce81f56bf82d268669720902cd37
JavaThugNGG <ildarjava@mail.ru>
2025-09-15 21:02:01 +0300

    feat(controller): add NewMatchController

--- TennisScoreboard/NewMatchController.cs
+++ TennisScoreboard/NewMatchController.cs
@@ -2,12 +2,50 @@
 
 namespace TennisScoreboard
 {
+    [Route("new-match")]
     public class NewMatchController : Controller
     {
-        [HttpGet("/new-match")]
-        public IActionResult ShowForm()
+        private readonly PlayerValidator _playerValidator;
+        private readonly ErrorDtoBuilder _errorDtoBuilder;
+        private readonly MatchPreparingService _matchPreparingService;
+        private readonly OngoingMatchesService _ongoingMatchesService;
+
+        public NewMatchController(PlayerValidator playerValidator, ErrorDtoBuilder errorDtoBuilder, MatchPreparingService matchPreparingService, OngoingMatchesService ongoingMatchesService)
         {
-            return View("NewMatch");
+            _playerValidator = playerValidator;
+            _errorDtoBuilder = errorDtoBuilder;
+            _matchPreparingService = matchPreparingService;
+            _ongoingMatchesService = ongoingMatchesService;
+        }
+
+        [HttpGet]
+        public IActionResult Index() => View("NewMatch");
+
+        [HttpPost]
+        public IActionResult Create(string playerOne, string playerTwo)
+        {
+            try
+            {
+                _playerValidator.ValidateName(playerOne);
+                _playerValidator.ValidateName(playerTwo);
+
+                MatchScoreModel matchScoreModel = _matchPreparingService.PersistPlayers(playerOne, playerTwo);
+                Guid pastedMatchId = _ongoingMatchesService.AddMatch(matchScoreModel);
+
+                //тут логгер
+
+                return Redirect($"/match-score?uuid={pastedMatchId}");//перепишешь потом через RedirectToAction когда контроллер другой появится
+            }
+            catch (IllegalPlayerNameException e)
+            {
+                ErrorDto error = _errorDtoBuilder.Build(e);
+
+                ViewData["ErrorMessage"] = error.Message;
+
+                //тут логгер
+
+                return View("NewMatch");
+            }
         }
     }
 }
