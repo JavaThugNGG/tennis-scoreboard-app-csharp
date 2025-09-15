ecef812ad6ee7bda335db0d4d0bf96b16a4bc0a7
JavaThugNGG <ildarjava@mail.ru>
2025-09-15 21:08:25 +0300

    feat(service): add MatchPreparingService

---
+++ TennisScoreboard/MatchPreparingService.cs
@@ -0,0 +1,31 @@
+﻿namespace TennisScoreboard
+{
+    public class MatchPreparingService
+    {
+        private readonly PlayerService _playerService;
+
+        public MatchPreparingService(PlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        public MatchScoreModel PersistPlayers(string firstPlayerName, string secondPlayerName)
+        {
+            int firstPlayerId = InsertPlayer(firstPlayerName);
+            int secondPlayerId = InsertPlayer(secondPlayerName);
+            return new MatchScoreModel(firstPlayerId, secondPlayerId);
+        }
+
+        private int InsertPlayer(string playerName)
+        {
+            try
+            {
+                return _playerService.Insert(playerName).Id;
+            }
+            catch (PlayerAlreadyExistsException ex)
+            {
+                return _playerService.GetByName(playerName).Id;
+            }
+        }
+    }
+}
