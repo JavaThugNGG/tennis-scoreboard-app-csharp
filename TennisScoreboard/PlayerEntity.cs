8c3040f8522ea41e4679e61b3a50b06e41ba29e2
JavaThugNGG <ildarjava@mail.ru>
2025-09-15 14:25:08 +0300

    chore(infrastructure): add entity classes for EF Core

---
+++ TennisScoreboard/PlayerEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TennisScoreboard
+{
+    public class PlayerEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List<MatchEntity> MatchesAsPlayer1 { get; set; } = new List<MatchEntity>();
+        public List<MatchEntity> MatchesAsPlayer2 { get; set; } = new List<MatchEntity>();
+        public List<MatchEntity> MatchesWon { get; set; } = new List<MatchEntity>();
+
+        public PlayerEntity() { }
+
+        public PlayerEntity(string name)
+        {
+            Name = name;
+        }
+    }
+}
