0d6d3e11601dba94579dcf325d87259e3383ad7b
JavaThugNGG <ildarjava@mail.ru>
2025-09-16 20:23:54 +0300

    feat(processor): add MatchProcessor

---
+++ TennisScoreboard/MatchProcessor.cs
@@ -0,0 +1,17 @@
+﻿namespace TennisScoreboard
+{
+    public class MatchProcessor
+    {
+        public MatchScoreModel FindMatch(IReadOnlyDictionary<Guid, MatchScoreModel> currentMatches, Guid guid)
+        {
+            MatchScoreModel currentMatch = currentMatches[guid];
+            if (currentMatch == null)
+            {
+                //лог ошибки
+                throw new PlayerNotFoundException("Матч не найден! uuid матча: " + guid);
+            }
+
+            return currentMatch;
+        }
+    }
+}
