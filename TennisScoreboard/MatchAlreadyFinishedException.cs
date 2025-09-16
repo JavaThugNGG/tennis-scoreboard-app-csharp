57a33c86ac85e18d7662b8afd3774c60160966a8
JavaThugNGG <ildarjava@mail.ru>
2025-09-16 20:30:18 +0300

    feat(exception): add MatchAlreadyFinishedException

--- TennisScoreboard/MatchAlreadyFinishedException.cs
+++ TennisScoreboard/MatchAlreadyFinishedException.cs
@@ -2,6 +2,6 @@
 {
     public class MatchAlreadyFinishedException : Exception
     {
-        public MatchAlreadyFinishedException(string message) : base(message) { }
+        public MatchAlreadyFinishedException() : base() { }
     }
 }
