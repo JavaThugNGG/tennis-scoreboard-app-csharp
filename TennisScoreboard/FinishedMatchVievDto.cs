6c37fdabc08e3c64e3a85d89c9d46113e72c8aed
JavaThugNGG <ildarjava@mail.ru>
2025-09-16 20:32:13 +0300

    feat(dto): add FinishedMatchViewDto

---
+++ TennisScoreboard/FinishedMatchVievDto.cs
@@ -0,0 +1,16 @@
+﻿namespace TennisScoreboard
+{
+    public class FinishedMatchViewDto
+    {
+        public MatchScoreModel CurrentMatch { get; init; }
+        public string FirstPlayerResult { get; init; }
+        public string SecondPlayerResult { get; init; }
+
+        public FinishedMatchViewDto(MatchScoreModel currentMatch, PlayersResultDto playersResult)
+        {
+            CurrentMatch = currentMatch;
+            FirstPlayerResult = playersResult.FirstPlayerResult;
+            SecondPlayerResult = playersResult.SecondPlayerResult;
+        }
+    }
+}
