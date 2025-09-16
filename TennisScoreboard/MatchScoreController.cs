9ab27be09113337c29d8aff4efc463cf67268443
JavaThugNGG <ildarjava@mail.ru>
2025-09-16 20:59:38 +0300

    feat(controller): add MatchScoreController

---
+++ TennisScoreboard/MatchScoreController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TennisScoreboard
+{
+    [Route("match-score")]
+    public class MatchScoreController : Controller
+    {
+        private readonly OngoingMatchesService _ongoingMatchesService;
+        private readonly MatchStateService _matchStateService;
+        private readonly MatchScoreCalculationService _matchScoreCalculationService;
+        private readonly FinishedMatchProcessingService _finishedMatchProcessingService;
+
+        private readonly MatchValidator _matchValidator;
+        private readonly PlayerValidator _playerValidator;
+
+        private readonly MatchParser _matchParser;
+        private readonly PlayerParser _playerParser;
+
+        private readonly MatchProcessor _matchProcessor;
+        private readonly PlayerProcessor _playerProcessor;
+
+        private readonly ErrorDtoBuilder _errorDtoBuilder;
+
+        public MatchScoreController(OngoingMatchesService ongoingMatchesService, MatchStateService matchStateService,
+            MatchScoreCalculationService matchScoreCalculationService,
+            FinishedMatchProcessingService finishedMatchProcessingService, MatchValidator matchValidator,
+            PlayerValidator playerValidator,
+            MatchParser matchParser, PlayerParser playerParser, MatchProcessor matchProcessor,
+            PlayerProcessor playerProcessor, ErrorDtoBuilder errorDtoBuilder)
+        {
+            _ongoingMatchesService = ongoingMatchesService;
+            _matchStateService = matchStateService;
+            _matchScoreCalculationService = matchScoreCalculationService;
+            _finishedMatchProcessingService = finishedMatchProcessingService;
+            _matchValidator = matchValidator;
+            _playerValidator = playerValidator;
+            _matchParser = matchParser;
+            _playerParser = playerParser;
+            _matchProcessor = matchProcessor;
+            _playerProcessor = playerProcessor;
+            _errorDtoBuilder = errorDtoBuilder;
+        }
+
+        [HttpGet]
+        public IActionResult ShowScore(string guid)
+        {
+            try
+            {
+                _matchValidator.ValidateGuid(guid);
+                Guid parsedGuid = _matchParser.ParseGuid(guid);
+                MatchScoreModel currentMatch =
+                    _matchProcessor.FindMatch(_ongoingMatchesService.CurrentMatches, parsedGuid);
+
+                return View(currentMatch);
+            }
+            catch (ArgumentException ex)
+            {
+                // Обработка некорректного UUID
+                ViewBag.ErrorMessage = ex.Message;
+                Response.StatusCode = 400; // Bad Request
+                return Redirect("new-match"); // тут должно быть существующее представление
+            }
+        }
+
+        
+        [HttpPost]
+        public IActionResult Scoring(string guid, string scoredPlayerId)
+        {
+            Guid matchGuid;
+            int scoredId;
+
+            try
+            {
+                _matchValidator.ValidateGuid(guid);
+                matchGuid = _matchParser.ParseGuid(guid);
+                _playerValidator.ValidateId(scoredPlayerId);
+                scoredId = _playerParser.ParseId(scoredPlayerId);
+            }
+            catch (ArgumentException ex)
+            {
+                return HandleIllegalArguments(ex, guid, scoredPlayerId);
+            }
+
+            IReadOnlyDictionary<Guid, MatchScoreModel> currentMatches = _ongoingMatchesService.CurrentMatches;
+            MatchScoreModel currentMatch = _matchProcessor.FindMatch(currentMatches, matchGuid);
+            PlayerSide scorerSide = _playerProcessor.DetermineScorerSide(currentMatch, scoredId);
+
+            try
+            {
+                _matchScoreCalculationService.Scoring(currentMatch, scorerSide);
+                _matchStateService.EnsureNotFinished(currentMatch);
+                //тут логгер
+                return Redirect("/match-score?guid=" + matchGuid);
+            }
+            catch (MatchAlreadyFinishedException ex)
+            {
+                return HandleFinishedMatch(currentMatch, scorerSide, matchGuid);
+            }
+        }
+
+        private IActionResult HandleFinishedMatch(MatchScoreModel currentMatch, PlayerSide scorerSide, Guid matchGuid)
+        {
+            lock (currentMatch.Lock)
+            {
+                if (!currentMatch.Finished)
+                {
+                    FinishedMatchViewDto finishedMatch = _finishedMatchProcessingService.HandleFinishedMatch(currentMatch, scorerSide, matchGuid);
+
+                    currentMatch.Finished = true;
+                    //тут логгер
+                    return View("MatchResult", finishedMatch);
+                }
+            }
+            return RedirectToAction("ShowScore", new { guid = matchGuid });
+        }
+
+        
+        private IActionResult HandleIllegalArguments(Exception ex, string guid, string scoredPlayerId)
+        {
+            var error = _errorDtoBuilder.Build(ex);
+            ViewData["ErrorMessage"] = error.Message;
+            return Redirect("NewMatch");//error не выводится!!!
+        }
+    }
+}
