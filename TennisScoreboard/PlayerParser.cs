f23f5e0bceb43c0881c42587ff27cbd8a86b8a47
JavaThugNGG <ildarjava@mail.ru>
2025-09-16 20:24:10 +0300

    feat(parser): add PlayerParser

---
+++ TennisScoreboard/PlayerParser.cs
@@ -0,0 +1,16 @@
+﻿namespace TennisScoreboard
+{
+    public class PlayerParser
+    {
+        public int ParseId(string playerId)
+        {
+            if (int.TryParse(playerId, out var result)) {
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("Ошибка при парсинге. Некорректный аргумент playerId :" + playerId);
+            }
+        }
+    }
+}
