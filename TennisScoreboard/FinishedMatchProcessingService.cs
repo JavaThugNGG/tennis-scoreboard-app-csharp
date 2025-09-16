6871a9a0b8f6d5a71bb05006318e32a405dc9e19
JavaThugNGG <ildarjava@mail.ru>
2025-09-16 20:32:41 +0300

    feat(service): add FinishedMatchProcessingService

---
+++ TennisScoreboard/FinishedMatchProcessingService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace TennisScoreboard
+{
+    public class FinishedMatchProcessingService
+    {
+        private readonly MatchFinishingService _matchFinishingService;
+        private readonly OngoingMatchesService _ongoingMatchesService;
+
+        public FinishedMatchProcessingService(MatchFinishingService matchFinishingService, OngoingMatchesService ongoingMatchesService)
+        {
+            _matchFinishingService = matchFinishingService;
+            _ongoingMatchesService = ongoingMatchesService;
+        }
+
+        public FinishedMatchViewDto HandleFinishedMatch(MatchScoreModel currentMatch, PlayerSide winnerSide, Guid matchGuid)
+        {
+            PlayersResultDto playersResult = _matchFinishingService.PersistMatch(currentMatch, winnerSide);
+            _ongoingMatchesService.RemoveMatchWithDelay(matchGuid, 1);
+            return new FinishedMatchViewDto(currentMatch, playersResult);
+        }
+    }
+}
