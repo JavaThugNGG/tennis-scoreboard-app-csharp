3422c6c7516f3351a73035de8c5cc2f079d8985f
JavaThugNGG <ildarjava@mail.ru>
2025-09-18 20:59:23 +0300

    feat(parser): add PageParser

---
+++ TennisScoreboard/PageParser.cs
@@ -0,0 +1,15 @@
+﻿namespace TennisScoreboard
+{
+    public class PageParser
+    {
+        public int ParsePage(string pageParameter)
+        {
+            if (int.TryParse(pageParameter, out int page))
+            {
+                return page;
+            }
+
+            throw new ArgumentException("Некорректный параметр страницы!");
+        }
+    }
+}
