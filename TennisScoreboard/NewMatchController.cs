e3a4f8d3a294590a8ec6fe9657c3c35304d6eeaa
JavaThugNGG <ildarjava@mail.ru>
2025-09-18 17:29:01 +0300

    fix(ui): correct error message rendering

--- TennisScoreboard/NewMatchController.cs
+++ TennisScoreboard/NewMatchController.cs
@@ -6,20 +6,18 @@ namespace TennisScoreboard
     public class NewMatchController : Controller
     {
         private readonly PlayerValidator _playerValidator;
-        private readonly ErrorDtoBuilder _errorDtoBuilder;
         private readonly MatchPreparingService _matchPreparingService;
         private readonly OngoingMatchesService _ongoingMatchesService;
 
-        public NewMatchController(PlayerValidator playerValidator, ErrorDtoBuilder errorDtoBuilder, MatchPreparingService matchPreparingService, OngoingMatchesService ongoingMatchesService)
+        public NewMatchController(PlayerValidator playerValidator, MatchPreparingService matchPreparingService, OngoingMatchesService ongoingMatchesService)
         {
             _playerValidator = playerValidator;
-            _errorDtoBuilder = errorDtoBuilder;
             _matchPreparingService = matchPreparingService;
             _ongoingMatchesService = ongoingMatchesService;
         }
 
         [HttpGet]
-        public IActionResult Index() => View("NewMatch");
+        public IActionResult ShowForm() => View("NewMatch");
 
         [HttpPost]
         public IActionResult Create(string playerOne, string playerTwo)
@@ -31,19 +29,14 @@ namespace TennisScoreboard
 
                 MatchScoreModel matchScoreModel = _matchPreparingService.PersistPlayers(playerOne, playerTwo);
                 Guid pastedMatchId = _ongoingMatchesService.AddMatch(matchScoreModel);
-
                 //тут логгер
-
-                return Redirect($"/match-score?uuid={pastedMatchId}");//перепишешь потом через RedirectToAction когда контроллер другой появится
+                return Redirect($"/match-score?guid={pastedMatchId}");// TODO:перепишешь потом через RedirectToAction когда контроллер другой появится
             }
-            catch (IllegalPlayerNameException e)
+            catch (IllegalPlayerNameException ex)
             {
-                ErrorDto error = _errorDtoBuilder.Build(e);
-
-                ViewData["ErrorMessage"] = error.Message;
-
+                ViewData["ErrorMessage"] = ex.Message;
+                Response.StatusCode = 400;
                 //тут логгер
-
                 return View("NewMatch");
             }
         }
