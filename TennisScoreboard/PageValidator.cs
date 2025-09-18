6254bf7a443220d77c6a157a979e971bb04f1822
JavaThugNGG <ildarjava@mail.ru>
2025-09-18 21:00:03 +0300

    feat(validator): add PageValidator

---
+++ TennisScoreboard/PageValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace TennisScoreboard
+{
+    public class PageValidator
+    {
+        public void ValidatePage(int page)
+        {
+            if (page < 1)
+            {
+                throw new ArgumentException("Некорректный аргумент страницы матча");
+            }
+        }
+    }
+}
