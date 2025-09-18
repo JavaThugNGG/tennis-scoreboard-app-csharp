8c98b6814da3e96633b9b384a7cc2b9dae1c8d10
JavaThugNGG <ildarjava@mail.ru>
2025-09-18 21:01:47 +0300

    feat(calculator): add MatchPageViewCalculator

---
+++ TennisScoreboard/MatchPageViewCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace TennisScoreboard
+{
+    public class MatchPageViewCalculator
+    {
+        private readonly int _matchesPerPage;
+
+        public MatchPageViewCalculator(int matchesPerPage)
+        {
+            _matchesPerPage = matchesPerPage;
+        }
+
+        public int GetStartIndex(int currentPage)
+        {
+            return (currentPage - 1) * _matchesPerPage;
+        }
+
+        public int GetTotalPages(int totalItems)
+        {
+            int totalPages = (int)Math.Ceiling((double)totalItems / _matchesPerPage);
+            return Math.Max(1, totalPages);
+        }
+    }
+}
