651028095422a8b0d0292b107681124e8a6127db
JavaThugNGG <ildarjava@mail.ru>
2025-09-20 20:55:39 +0300

    feat(controller): add MatchesController

---
+++ TennisScoreboard/MatchesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TennisScoreboard
+{
+    [Route("matches")]
+    public class MatchesController : Controller
+    {
+        private readonly MatchPageViewService _matchPageViewService;
+
+        public MatchesController(MatchPageViewService matchPageViewService)
+        {
+            _matchPageViewService = matchPageViewService;
+        }
+
+        [HttpGet]
+        public IActionResult Show(string page, string playerNameFilter)
+        {
+            MatchPageViewDto matchPage;
+
+            try
+            {
+                matchPage = _matchPageViewService.GetPage(page, playerNameFilter);
+            }
+            catch (IllegalPlayerNameFilterException ex)
+            {
+                //лог
+                matchPage = GetPageWithoutFilter(page);
+
+                Response.StatusCode = 400;
+                ViewData["ErrorMessage"] = ex.Message;
+            }
+            return View("Matches", matchPage);
+        }
+
+        private MatchPageViewDto GetPageWithoutFilter(string page)
+        {
+            return _matchPageViewService.GetPage(page, null);
+        }
+    }
+}
+
+
