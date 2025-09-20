d15f816b7322b312db97ded03a52fc65e18a8c64
JavaThugNGG <ildarjava@mail.ru>
2025-09-20 20:47:05 +0300

    feat(dao): add MatchDao

---
+++ TennisScoreboard/MatchDao.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TennisScoreboard
+{
+    public class MatchDao
+    {
+        public IList<MatchEntity> GetPage(AppDbContext context, int matchesPerPage, int paginationStartIndex)
+        {
+            return context.Matches
+                .Include(m => m.Player1)
+                .Include(m => m.Player2)
+                .Include(m => m.Winner)
+                .OrderByDescending(m => m.Id)
+                .Skip(paginationStartIndex)
+                .Take(matchesPerPage)
+                .ToList();
+        }
+
+        public List<MatchEntity> GetPageWithPlayerFilter(AppDbContext context, PlayerEntity player, int matchesPerPage, int paginationStartIndex)
+        {
+            return context.Matches
+                .Include(m => m.Player1)
+                .Include(m => m.Player2)
+                .Include(m => m.Winner)
+                .Where(m => m.Player1 == player || m.Player2 == player)
+                .OrderByDescending(m => m.Id)
+                .Skip(paginationStartIndex)
+                .Take(matchesPerPage)
+                .ToList();
+        }
+
+        public long Count(AppDbContext context)
+        {
+            return context.Matches.LongCount();
+        }
+
+        public long CountWithPlayerFilter(AppDbContext context, PlayerEntity player)
+        {
+            return context.Matches
+                .Where(m => m.Player1 == player || m.Player2 == player)
+                .LongCount();
+        }
+    }
+}
