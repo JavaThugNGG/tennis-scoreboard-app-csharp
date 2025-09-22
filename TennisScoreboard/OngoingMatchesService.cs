ebd13a704f94a83f12bcb6f01610ee08bf5dfa3b
JavaThugNGG <ildarjava@mail.ru>
2025-09-22 13:43:33 +0300

    chore(service): add logging to OngoingMatchesService

--- TennisScoreboard/OngoingMatchesService.cs
+++ TennisScoreboard/OngoingMatchesService.cs
@@ -5,10 +5,17 @@ public class OngoingMatchesService
 {
     private readonly ConcurrentDictionary<Guid, MatchScoreModel> _currentMatches = new();
     private readonly ConcurrentDictionary<Guid, bool> _persistedMatches = new();
-    private readonly MatchScheduler _scheduler = new();
+    private readonly MatchScheduler _scheduler;
+    private readonly ILogger<OngoingMatchesService> _logger;
 
     public IReadOnlyDictionary<Guid, MatchScoreModel> CurrentMatches => _currentMatches;
     public IReadOnlyDictionary<Guid, bool> PersistedMatches => _persistedMatches;
+
+    public OngoingMatchesService(MatchScheduler scheduler, ILogger<OngoingMatchesService> logger)
+    {
+        _scheduler = scheduler;
+        _logger = logger;
+    }
 
     public Guid AddMatch(MatchScoreModel match)
     {
@@ -23,6 +30,7 @@ public class OngoingMatchesService
         {
             _currentMatches.TryRemove(id, out _);
             _persistedMatches.TryRemove(id, out _);
+            _logger.LogInformation("Match {MatchId} removed successfully after {DelaySeconds}s", id, delaySeconds);
         }, delaySeconds);
     }
 
@@ -34,5 +42,6 @@ public class OngoingMatchesService
     public void MarkMatchAsPersisted(Guid matchId)
     {
         _persistedMatches[matchId] = true;
+        _logger.LogInformation("Match {MatchId} marked as persisted", matchId);
     }
 }
