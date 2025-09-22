9a54960f339f0b5f119cc204841c8b8ad79660a9
JavaThugNGG <ildarjava@mail.ru>
2025-09-22 13:52:25 +0300

    chore(infrastructure): add logging to DatabaseInitializer

--- TennisScoreboard/DatabaseInitializer.cs
+++ TennisScoreboard/DatabaseInitializer.cs
@@ -1,29 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
 
 namespace TennisScoreboard
 {
     public class DatabaseInitializer
     {
         private readonly IDbContextFactory<AppDbContext> _factory;
+        private readonly ILogger<DatabaseInitializer> _logger;
 
-        public DatabaseInitializer(IDbContextFactory<AppDbContext> factory)
+        public DatabaseInitializer(IDbContextFactory<AppDbContext> factory, ILogger<DatabaseInitializer> logger)
         {
             _factory = factory;
+            _logger = logger;
         }
 
         public void Init(WebApplication app)
         {
             using var context = _factory.CreateDbContext();
-            context.Database.EnsureCreated();
 
-            Debug.WriteLine("Таблицы в базе:");//в лог
+            _logger.LogInformation("Ensuring database is created...");
+            context.Database.EnsureCreated();
+            _logger.LogInformation("Database ensured.");
+
+            _logger.LogInformation("Tables in database:");
             var tables = context.Model.GetEntityTypes()
                 .Select(t => t.GetTableName())
                 .Distinct();
 
             foreach (var table in tables)
-                Debug.WriteLine(table);
+            {
+                _logger.LogInformation(table);
+            }
         }
     }
 }
