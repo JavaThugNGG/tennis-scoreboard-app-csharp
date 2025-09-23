601f51099737ff4361a4a0c042fbc41a9fc15433
JavaThugNGG <ildarjava@mail.ru>
2025-09-23 18:26:22 +0300

    chore(controller): add logging to NewMatchController

--- TennisScoreboard/NewMatchController.cs
+++ TennisScoreboard/NewMatchController.cs
@@ -8,12 +8,14 @@ namespace TennisScoreboard
         private readonly PlayerValidator _playerValidator;
         private readonly MatchPreparingService _matchPreparingService;
         private readonly OngoingMatchesService _ongoingMatchesService;
+        private readonly ILogger<NewMatchController> _logger;
 
-        public NewMatchController(PlayerValidator playerValidator, MatchPreparingService matchPreparingService, OngoingMatchesService ongoingMatchesService)
+        public NewMatchController(PlayerValidator playerValidator, MatchPreparingService matchPreparingService, OngoingMatchesService ongoingMatchesService, ILogger<NewMatchController> logger)
         {
             _playerValidator = playerValidator;
             _matchPreparingService = matchPreparingService;
             _ongoingMatchesService = ongoingMatchesService;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -29,13 +31,13 @@ namespace TennisScoreboard
 
                 MatchScoreModel matchScoreModel = _matchPreparingService.PersistPlayers(playerOne, playerTwo);
                 Guid pastedMatchId = _ongoingMatchesService.AddMatch(matchScoreModel);
-                //тут логгер
+                _logger.LogInformation("Match was created, id: {}, first player id: {}, second player id: {}", pastedMatchId, matchScoreModel.FirstPlayerId, matchScoreModel.SecondPlayerId);
                 return RedirectToAction("ShowScore", "MatchScore", new { guid = pastedMatchId });
             }
             catch (IllegalPlayerNameException ex)
             {
                 ViewData["ErrorMessage"] = ex.Message;
-                //тут логгер
+                _logger.LogWarning(ex, "Incorrect name of first or second player: {}, {}", playerOne, playerTwo);
                 return View("NewMatch");
             }
         }
